7d4235ff9ea201e6400242e9e481ad2c067d7b76
pedroraposoalves <pedro.m.raposoalves@gmail.com>
2025-01-11 00:24:47 +0000

    push

--- srcs/builtins/bl_env.c
+++ srcs/builtins/bl_env.c
@@ -6,7 +6,7 @@
 /*   By: pemirand <pemirand@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 13:15:06 by pemirand          #+#    #+#             */
-/*   Updated: 2025/01/11 00:23:27 by pemirand         ###   ########.fr       */
+/*   Updated: 2025/01/11 00:23:54 by pemirand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@ int	ft_env(t_main *pgr, char **argv)
 {
 	int	i;
 
-	if matrix_len(argv) > 2)
-		return(print_error_errno(SHELL_NAME, argv[2], NULL));
+	if (matrix_len(argv) > 2)
+		return (print_error_errno(SHELL_NAME, argv[2], NULL));
 	if (!pgr->cur_envp)
 		return (print_error(SHELL_NAME, "env", NULL, "Env variable empty"), \
 			EXIT_FAILURE);
