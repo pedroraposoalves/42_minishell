de043bfda596ad266374317973ad760239640c8c
Matheus Alves Barbosa Castro <malves-b@c2r6s7.42porto.com>
2025-01-11 11:00:05 +0000

    update main

--- srcs/minishell.c
+++ srcs/minishell.c
@@ -6,7 +6,7 @@
 /*   By: malves-b <malves-b@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 16:04:09 by pemirand          #+#    #+#             */
-/*   Updated: 2025/01/10 20:19:26 by malves-b         ###   ########.fr       */
+/*   Updated: 2025/01/11 10:59:38 by malves-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,8 +51,8 @@ int	main(int argc, char **argv, char **envp)
 
 	start = NULL;
 	(void) argv;
-	pgr = init_main(envp);
 	check_have_argument(argc);
+	pgr = init_main(envp);
 	while (1)
 	{
 		setup_signals();
